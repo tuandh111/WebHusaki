ffd5c2de566e62f3301ec4ab27595f86723b7f9a
dotantai208 <dotantai2008@gmail.com>
2024-01-10 10:41:21 +0700

    tai cmmm

---
+++ demo/src/main/java/asm/osaki/OsakiApplication.java
@@ -0,0 +1,13 @@
+package asm.osaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsakiApplication.class, args);
+	}
+
+}
