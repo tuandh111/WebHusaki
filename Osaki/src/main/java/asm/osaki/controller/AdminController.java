1b6f7aebfaf2172588a8563f57c56de81b2941ef
TrungAug <trung2894@gmail.com>
2024-01-28 16:01:32 +0700

    adjust route /admin

--- Osaki/src/main/java/asm/osaki/controller/AdminController.java
+++ Osaki/src/main/java/asm/osaki/controller/AdminController.java
@@ -1,14 +1,60 @@
 package asm.osaki.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
+@RequestMapping("admin")
 public class AdminController {
 
-	@GetMapping("admin")
-	public String getHome() {
+	@GetMapping
+	public String getHome(Model model) {
+		
+		model.addAttribute("content","_content.jsp");
+		return "admin/admin";
+	}
+	@GetMapping("account-manager")
+	public String accountManager(Model model) {
+		model.addAttribute("content","_content-account.jsp");
+		return "admin/admin";
+	}
+	
+	@GetMapping("category-manager")
+	public String categoryManager(Model model) {
+		model.addAttribute("content","_content-category.jsp");
+		return "admin/admin";
+	}
+	
+	@GetMapping("product-manager")
+	public String productManager(Model model) {
+		model.addAttribute("content","_content-product.jsp");
+		return "admin/admin";
+	}
+	
+	@GetMapping("revenue-statistics")
+	public String revenueStatistics(Model model) {
+		model.addAttribute("content","_content-revenue-statistics.jsp");
+		return "admin/admin";
+	}
+	
+	@GetMapping("product-statistics")
+	public String productStatistics(Model model) {
+		model.addAttribute("content","_content-product-statistics.jsp");
+		return "admin/admin";
+	}
+	
+	@GetMapping("account-statistics")
+	public String accountStatistics(Model model) {
+		model.addAttribute("content","_content-account-statistics.jsp");
+		return "admin/admin";
+	}
+	
+	@GetMapping("category-statistics")
+	public String categoryStatistics(Model model) {
+		model.addAttribute("content","_content-category-statistics.jsp");
 		return "admin/admin";
 	}
 }
---
+++ Osaki/src/main/webapp/WEB-INF/views/admin/_content-category-statistics.jsp
@@ -0,0 +1,3 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<h1>Form Thống kê doanh mục sản phẩm: Số lượng sản phẩm theo từng doanh mục,....</h1>
