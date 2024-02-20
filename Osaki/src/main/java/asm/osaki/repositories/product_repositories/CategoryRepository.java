db204b3ea7547de111dfc971ee95615a15acf6f0
tuandh12345 <129180520+tuandh12345@users.noreply.github.com>
2024-02-20 09:47:12 +0700

    Update CategoryRepository.java

--- Osaki/src/main/java/asm/osaki/repositories/product_repositories/CategoryRepository.java
+++ Osaki/src/main/java/asm/osaki/repositories/product_repositories/CategoryRepository.java
@@ -13,10 +13,10 @@ public interface CategoryRepository extends JpaRepository<Category,Integer> {
 	List<Category> findByCategoryNameContainingIgnoreCase(String categoryName);
 	//List<CategoryAndCount> findByCategoryNameContainingIgnoreCase(String categoryName);
 		
-	@Query("select c, size(c.products) from Category c")
+	@Query("select c, size(c.products) from category c")
 	List<Object[]> findAllAndCount();
 	
-	@Query("select c, size(c.products) from Category c where c.categoryName like %?1%")
+	@Query("select c, size(c.products) from category c where c.categoryName like %?1%")
 	List<Object[]> findByCategoryAndCountName(String name);
 	
 }
